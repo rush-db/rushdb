743aa3cd259d544ff260d5761b7d285fa2fc7976
h3yAlias <asthmaisback@gmail.com>
2025-01-25 13:25:52 +0300

    chore: removed unused constant

--- platform/core/src/dashboard/billing/stripe/interfaces/stripe.constans.ts
+++ platform/core/src/dashboard/billing/stripe/interfaces/stripe.constans.ts
@@ -1,38 +1,3 @@
-import { isDevMode } from '@/common/utils/isDevMode'
-import { TPlan } from '@/dashboard/billing/stripe/interfaces/stripe.types'
-
-// @TODO: Move to env
-export const PRODUCT_PLAN_MAP: TPlan =
-  isDevMode() ?
-    {
-      pro: {
-        month: {
-          amount: 11,
-          priceId: 'price_1PhomaJKGwDMYurpuo4gmg7B',
-          productId: 'prod_QYwfBSZk27uH2n'
-        },
-        annual: {
-          amount: 99,
-          priceId: 'price_1PhokbJKGwDMYurpab9nLnDX',
-          productId: 'prod_QYwd1G0GI2cUIa'
-        }
-      }
-    }
-  : {
-      pro: {
-        month: {
-          amount: 19,
-          priceId: 'price_1QflgOJKGwDMYurpVSD8neET',
-          productId: 'prod_RYtTNwQNaP025W'
-        },
-        annual: {
-          amount: 199,
-          priceId: 'price_1QfcwoJKGwDMYurplLpyerlu',
-          productId: 'prod_RYkRuqwNDrnyr7'
-        }
-      }
-    }
-
 export enum EConfigKeyByPlan {
   pro = '_PRO'
 }
