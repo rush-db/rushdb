46d88d4ba31038cf56b70e177d96ac9b9ae6b222
h3yAlias <asthmaisback@gmail.com>
2025-05-19 00:51:04 +0300

    fix: removed some b-e logic which protected invitation flow

--- platform/core/src/dashboard/user/interfaces/user-properties.interface.ts
+++ platform/core/src/dashboard/user/interfaces/user-properties.interface.ts
@@ -2,16 +2,8 @@ import { TUserProperties } from '../model/user.interface'
 
 type TIUserAuthProperties = 'googleAuth' | 'githubAuth' | 'password'
 
-export type AcceptWorkspaceInvitationParams<T extends boolean = boolean> =
-  T extends true ?
-    {
-      forceUserSignUp: true
-      inviteToken: string
-      userData: Omit<TUserProperties, 'id' | 'isEmail'>
-    }
-  : {
-      forceUserSignUp?: false
-      inviteToken: string
-      authUserLogin: string
-    }
+export type AcceptWorkspaceInvitationParams = {
+  inviteToken: string
+  authUserLogin: string
+}
 export interface IUserProperties extends Omit<TUserProperties, TIUserAuthProperties> {}
